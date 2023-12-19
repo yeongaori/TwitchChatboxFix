eee987c645dc32f885db3e7edfba97c1899d643c
yeongaori <55352400+yeongaori@users.noreply.github.com>
2023-12-19 14:57:48 +0900

    Update chzzk-chat.user.js

--- chzzk-chat.user.js
+++ chzzk-chat.user.js
@@ -38,5 +38,5 @@ const observer = new MutationObserver(handleMutations);
 observer.observe(document, { childList: true, subtree: true });
 
 function chatUpdated(name, message){
-    console.log("Name: " + name + ", Message: " + message);
+    console.log("Name: " + name + "\nMessage: " + message);
 }
