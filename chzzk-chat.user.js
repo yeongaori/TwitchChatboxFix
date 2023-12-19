c2d79e5d89b75ed7a740c340fbd7234b3902c9ec
yeongaori <55352400+yeongaori@users.noreply.github.com>
2023-12-19 18:46:29 +0900

    Update chzzk-chat.user.js

--- chzzk-chat.user.js
+++ chzzk-chat.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CHZZK Chat
 // @namespace    https://github.com/yeongaori/userscript
-// @version      1.0
+// @version      1.1
 // @updateURL    https://github.com/yeongaori/userscript/raw/master/chzzk-chat.user.js
 // @downloadURL  https://github.com/yeongaori/userscript/raw/master/chzzk-chat.user.js
 // @description  CHZZK chat test
@@ -9,7 +9,7 @@
 // @match        https://chzzk.naver.com/live/*
 // @icon         https://ssl.pstatic.net/static/nng/glive/icon/favicon.png
 // @grant        none
-// @require      none
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 // ==/UserScript==
 
 function handleMutations(mutationsList) {
@@ -19,13 +19,42 @@ function handleMutations(mutationsList) {
                 const classes = node.classList;
                 for (let className of classes) {
                     if (className.startsWith('live_chatting_list_item')) {
-                        const nameTextElements = node.querySelectorAll('[class^="name_text"]');
-                        const messageTextElements = node.querySelectorAll('[class^="live_chatting_message_text"]');
-                        nameTextElements.forEach(nameElement => {
-                            const name = nameElement.textContent.trim();
-                            messageTextElements.forEach(messageElement => {
-                                const message = messageElement.textContent.trim();
-                                chatUpdated(name, message);
+                        const chatNameTextElements = node.querySelectorAll('[class^="name_text"]');
+                        const chatMessageTextElements = node.querySelectorAll('[class^="live_chatting_message_text"]');
+                        const chatBadgeImgElements = node.querySelectorAll('[class^="badge_container"] img');
+                        chatNameTextElements.forEach(chatNameElement => {
+                            const name = chatNameElement.textContent.trim();
+                            chatMessageTextElements.forEach(chatMessageElement => {
+                                const message = chatMessageElement.textContent.trim();
+                                let isSubscribed = false;
+                                chatBadgeImgElements.forEach(chatBadgeElement => {
+                                    const badge = chatBadgeElement.alt.trim();
+                                    if( badge == "입덕 완료" ){
+                                        isSubscribed = true;
+                                    }
+                                });
+                                chatUpdated(name, message, isSubscribed);
+                            });
+                        });
+                        const donationNameTextElements = node.querySelectorAll('[class^="name_text"]');
+                        const donationMessageTextElements = node.querySelectorAll('[class^="live_chatting_donation_message_text"]');
+                        const donationBadgeImgElements = node.querySelectorAll('[class^="badge_container"] img');
+                        const donationMoneyElements = node.querySelectorAll('[class^="live_chatting_donation_message_money"]');
+                        donationNameTextElements.forEach(donationNameElement => {
+                            const name = donationNameElement.textContent.trim();
+                            donationMessageTextElements.forEach(donationMessageElement => {
+                                const message = donationMessageElement.textContent.trim();
+                                let isSubscribed = false;
+                                donationBadgeImgElements.forEach(donationBadgeElement => {
+                                        const badge = donationBadgeElement.alt.trim();
+                                        if( badge == "입덕 완료" ){
+                                            isSubscribed = true;
+                                        }
+                                        donationMoneyElements.forEach(donationMoneyElement => {
+                                            const money = parseInt(donationMoneyElement.textContent.trim().replaceAll(",", ""));
+                                            donationUpdated(name, message, isSubscribed, money);
+                                        });
+                                });
                             });
                         });
                     }
@@ -34,9 +63,16 @@ function handleMutations(mutationsList) {
         });
     }
 }
+
 const observer = new MutationObserver(handleMutations);
 observer.observe(document, { childList: true, subtree: true });
 
-function chatUpdated(name, message){
-    //put your code here
+function chatUpdated(name, message, isSubscribed){
+    //console.log("CHAT\nName: " + name + "\nMessage: " + message + "\nisSubscribed: " + isSubscribed);
+    //your code here
+}
+
+function donationUpdated(name, message, isSubscribed, money){
+    //console.log("DONATION\nName: " + name + "\nMessage: " + message + "\nisSubscribed: " + isSubscribed + "\nMoney: " + money);
+    //your code here
 }
