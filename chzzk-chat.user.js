c5adf91f4da9486d7addac9e37c88f950f5300a5
yeongaori <55352400+yeongaori@users.noreply.github.com>
2023-12-21 01:56:37 +0900

    Added WebSocket support

--- chzzk-chat.user.js
+++ chzzk-chat.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CHZZK Chat
 // @namespace    https://github.com/yeongaori/userscript
-// @version      1.1.1
+// @version      1.2.0
 // @updateURL    https://github.com/yeongaori/userscript/raw/master/chzzk-chat.user.js
 // @downloadURL  https://github.com/yeongaori/userscript/raw/master/chzzk-chat.user.js
 // @description  CHZZK chat test
@@ -12,6 +12,9 @@
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 // ==/UserScript==
 
+const url = window.location.href;
+const channelId = url.split('https://chzzk.naver.com/live/')[1].split('/')[0];
+
 function handleMutations(mutationsList) {
     for (let mutation of mutationsList) {
         mutation.addedNodes.forEach(node => {
@@ -19,6 +22,7 @@ function handleMutations(mutationsList) {
                 const classes = node.classList;
                 for (let className of classes) {
                     if (className.startsWith('live_chatting_list_item')) {
+
                         const chatNameTextElements = node.querySelectorAll('[class^="name_text"]');
                         const chatMessageTextElements = node.querySelectorAll('[class^="live_chatting_message_text"]');
                         const chatBadgeImgElements = node.querySelectorAll('[class^="badge_container"] img');
@@ -29,13 +33,14 @@ function handleMutations(mutationsList) {
                                 let hasBadge = false;
                                 chatBadgeImgElements.forEach(chatBadgeElement => {
                                     const badge = chatBadgeElement.alt.trim();
-                                    if( badge ){
+                                    if( badge ) {
                                         hasBadge = true;
                                     }
                                 });
                                 chatUpdated(name, message, hasBadge);
                             });
                         });
+
                         const donationNameTextElements = node.querySelectorAll('[class^="name_text"]');
                         const donationMessageTextElements = node.querySelectorAll('[class^="live_chatting_donation_message_text"]');
                         const donationBadgeImgElements = node.querySelectorAll('[class^="badge_container"] img');
@@ -57,6 +62,7 @@ function handleMutations(mutationsList) {
                                 });
                             });
                         });
+
                     }
                 }
             }
@@ -66,6 +72,44 @@ function handleMutations(mutationsList) {
 const observer = new MutationObserver(handleMutations);
 observer.observe(document, { childList: true, subtree: true });
 
+var originalWebSocket = window.WebSocket;
+window.WebSocket = function(url, protocols) {
+    if (url.includes('chat')) {
+        console.log('WebSocket connection intercepted from chat URL: ' + url);
+        var ws = new originalWebSocket(url, protocols);
+
+        const originalSend = ws.send;
+        ws.send = function(data) {
+            const sentData = JSON.parse(data);
+            //console.log('Intercepted data being sent');
+            //console.log(sentData);
+            originalSend.call(ws, data);
+        };
+
+        ws.onmessage = function(event) {
+            const eventData = JSON.parse(event.data);
+            const profileData = JSON.parse(eventData.bdy[0].profile);
+            webSocketReceived(
+                profileData.nickname,
+                profileData.userIdHash,
+                eventData.bdy[0].msg,
+                eventData.bdy[0].msgTypeCode,
+                profileData.profileImageUrl,
+                profileData.userRoleCode,
+                profileData.badge,
+                profileData.title,
+                profileData.verifiedMark,
+                profileData.activityBadges,
+                profileData.streamingProperty
+            );
+            //console.log('Intercepted data being received');
+            //console.log(eventData);
+        };
+        return ws;
+    }
+    return new originalWebSocket(url, protocols);
+};
+
 function chatUpdated(name, message, hasBadge){
     //console.log("CHAT\nName: " + name + "\nMessage: " + message + "\nhasBadge: " + hasBadge);
     //your code here
@@ -74,4 +118,9 @@ function chatUpdated(name, message, hasBadge){
 function donationUpdated(name, message, hasBadge, money){
     //console.log("DONATION\nName: " + name + "\nMessage: " + message + "\nhasBadge: " + hasBadge + "\nMoney: " + money);
     //your code here
+}
+
+function webSocketReceived(name, userIdHash, message, msgTypeCode, extraData, profileImageUrl, userRoleCode, badge, title, verifiedMark, activityBadges, streamingProperty){
+    //if isDonation is true and you want to get donation's information, get those from extraData
+    //msgTypeCode: 1 for normal chats, and 10 for donations
 }
